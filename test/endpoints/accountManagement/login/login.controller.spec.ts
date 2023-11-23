d72ea963fc135f94189984701c5ec030531702af
guyb20 <guy.berenshten@gmail.com>
2023-11-23 23:38:05 +0200

    updated login controller unit tests to match other unit tests convention

--- test/endpoints/accountManagement/login/login.controller.spec.ts
+++ test/endpoints/accountManagement/login/login.controller.spec.ts
@@ -1,81 +1,80 @@
-import { Response } from 'express';
-import { LoginController } from 'src/endpoints/accountManagement/login/login.controller';
-import { LoginWebsiteDto } from 'src/models/dtos/accountManagement/loginWebsiteDto';
-import { OpResult } from 'src/models/response/OpResult';
-import { Test, TestingModule } from '@nestjs/testing';
-import { LoginService } from 'src/endpoints/accountManagement/login/login.service';
-import { serverResponses } from 'src/static/ServerResponses';
+import { Response } from 'express'
+import { LoginController } from 'src/endpoints/accountManagement/login/login.controller'
+import { LoginWebsiteDto } from 'src/models/dtos/accountManagement/loginWebsiteDto'
+import { OpResult } from 'src/models/response/OpResult'
+import { Test, TestingModule } from '@nestjs/testing'
+import { LoginService } from 'src/endpoints/accountManagement/login/login.service'
+import { serverResponses } from 'src/static/ServerResponses'
 
 describe('login.controller', () => {
-  let controller: LoginController;
-  let loginServiceStub;
+  let controller: LoginController
+  let loginServiceStub: LoginService
 
   const loginWebsiteDto : LoginWebsiteDto = {
     username: 'testuser',
     password: 'testpassword',
-  };
+  }
 
   const responseMock = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
-  } as any as Response;
+  } as any as Response
 
   beforeEach(async () => {
-    loginServiceStub = {
-      isLoginValid: jest.fn(),
-      login: jest.fn(),
-    };
-
     const module : TestingModule = await Test.createTestingModule({
       controllers: [LoginController],
       providers: [
         {
           provide: LoginService,
-          useValue: loginServiceStub
+          useValue: {
+            isLoginValid: jest.fn(),
+            login: jest.fn(),
+          }
         }
       ],
-    }).compile();
+    }).compile()
 
-    controller = module.get(LoginController);
-  });
+    controller = module.get(LoginController)
+    loginServiceStub = module.get(LoginService)
+  })
 
   it('should return a session ID on successful login with status 200', async () => {
     // Arrange
-    const expectedSessionId = 'adifjsdf';
-    loginServiceStub.isLoginValid = jest.fn().mockResolvedValue(OpResult.SUCCESS);
-    loginServiceStub.login = jest.fn().mockResolvedValue(expectedSessionId);
+    const expectedSessionId = 'adifjsdf'
+    loginServiceStub.isLoginValid = jest.fn().mockResolvedValue(OpResult.SUCCESS)
+    loginServiceStub.login = jest.fn().mockResolvedValue(expectedSessionId)
 
     // Act
-    await controller.register(loginWebsiteDto, responseMock);
+    await controller.register(loginWebsiteDto, responseMock)
 
     // Assert
-    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.success.status);
-    expect(responseMock.json).toHaveBeenCalledWith({sessionId: expectedSessionId});
-  });
+    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.success.status)
+    expect(responseMock.json).toHaveBeenCalledWith({sessionId: expectedSessionId})
+  })
 
   it('should return user not exist with status code 404 on non-existent user', async () => {
     // Arrange
     loginServiceStub.isLoginValid = jest
       .fn()
-      .mockResolvedValue(OpResult.USER_NOT_EXIST);
+      .mockResolvedValue(OpResult.USER_NOT_EXIST)
 
     // Act
-    await controller.register(loginWebsiteDto, responseMock);
+    await controller.register(loginWebsiteDto, responseMock)
 
     // Assert
-    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.userNotExist.status);
-    expect(responseMock.json).toHaveBeenCalledWith({message: serverResponses.userNotExist.message});
-  });
+    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.userNotExist.status)
+    expect(responseMock.json).toHaveBeenCalledWith({message: serverResponses.userNotExist.message})
+  })
 
   it('should return 401 and message on incorrect username/password', async () => {
     // Arrange
-    loginServiceStub.isLoginValid = jest.fn().mockResolvedValue(OpResult.INCORRECT_PASSWORD);
+    loginServiceStub.isLoginValid = jest.fn().mockResolvedValue(OpResult.INCORRECT_PASSWORD)
 
     // Act
-    await controller.register(loginWebsiteDto, responseMock);
+    await controller.register(loginWebsiteDto, responseMock)
 
     // Assert
-    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.incorrectPassword.status);
-    expect(responseMock.json).toHaveBeenCalledWith({ message: serverResponses.incorrectPassword.message});
-  });
-});
+    expect(responseMock.status).toHaveBeenCalledWith(serverResponses.incorrectPassword.status)
+    expect(responseMock.json).toHaveBeenCalledWith({ message: serverResponses.incorrectPassword.message})
+  })
+})
