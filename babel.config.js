0b359bf46fc34bd8b59820368ddc2d4da57bef8b
guyb20 <guy.berenshten@gmail.com>
2023-11-24 01:02:51 +0200

    removed babel

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    presets: ["@babel/preset-env", "@babel/preset-typescript"],
-    plugins: [
-        ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
-        ["@babel/plugin-syntax-decorators", { decoratorsBeforeExport: true }]
-    ]
-};
